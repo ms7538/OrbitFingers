1792675f4e08dcae806fcd0e64fedc3fa18cc576
Marko Simunac <MSimunac3@gatech.edu>
2015-07-23 12:59:54 -0700

    added orbit color reactions to theta

--- app/src/main/java/poloapps/orbitfingers/SOFview.java
+++ app/src/main/java/poloapps/orbitfingers/SOFview.java
@@ -41,8 +41,10 @@ public class SOFview extends View {
     private float flrdB1 = (float) B1dist;
     private float flrdB2 = (float) B2dist;
     private String MBclr="#ffea7d";
-    private String B2color="#017ed5";
-    private String B1color="#017ed5";
+    private String Blue1="#017ed5";
+    private String Currcol= Blue1;
+    private String Green1="#00ff00";
+    private String Red1="#ff0000";
     private  double thcns=.5;
     private double theta=180;
     private  double thcns2=.99;
@@ -81,15 +83,13 @@ public class SOFview extends View {
         //String unitType = getContext().getString(R.string.pref_units_key);
         paint.setStyle(Paint.Style.STROKE);
 
-        paint.setColor(Color.parseColor(B1color));
-        canvas.drawCircle(CX, CY, flrdB1, paint);
-        paint.setColor(Color.parseColor(B1color));
-        canvas.drawCircle(CX, CY, flrdB2, paint);
+        orbit(canvas, paint, Currcol, CX, CY, flrdB1);
+        orbit(canvas, paint, Currcol, CX, CY, flrdB2);
         // Draw Planets
         paint.setStyle(Paint.Style.FILL);
         drawball(canvas, ballBounds, MBx, MBsze, MBy, paint, MBclr);
-        drawball(canvas, ballBounds, B1X, Bsize, B1y, paint, B1color);
-        drawball(canvas, ballBounds, B2X, Bsize, B2y, paint, B2color);
+        drawball(canvas, ballBounds, B1X, Bsize, B1y, paint, Blue1);
+        drawball(canvas, ballBounds, B2X, Bsize, B2y, paint, Blue1);
         paint.setTypeface(Typeface.MONOSPACE);
         paint.setTextSize(12);
        String str="POS1";
@@ -107,6 +107,11 @@ public class SOFview extends View {
         // update();
     }
 
+    private static void orbit(Canvas canvas, Paint paint, String blue1, int CX, int CY, float flrdB1) {
+        paint.setColor(Color.parseColor(blue1));
+        canvas.drawCircle(CX, CY, flrdB1, paint);
+    }
+
     private static void drawball(Canvas canvas, RectF ballBounds, float MBx, float MBsze, float MBy, Paint paint, String MBclr) {
         ballBounds.set(MBx - MBsze, MBy - MBsze, MBx + MBsze, MBy + MBsze);
         paint.setColor(Color.parseColor(MBclr));
@@ -115,7 +120,7 @@ public class SOFview extends View {
 
     private void txtcnvs(Canvas canvas, String str, int x, int y) {
         formatter.format(str);
-        paint.setColor(Color.parseColor(B1color));
+        paint.setColor(Color.parseColor(Blue1));
         canvas.drawText(statusMsg.toString(), x, y, paint);
         statusMsg.delete(0, statusMsg.length()); // Empty buffer
     }
@@ -128,9 +133,13 @@ public class SOFview extends View {
 
             case MotionEvent.ACTION_DOWN:
                 if((event.getX() >= CX-30 && event.getX() <= CX+30 && event.getY() >= 250 && event.getY() < 350) && Mch==10) {
-
+                    Currcol=Green1;
                     Log.i("theta", Double.toString(Mch));
+                }else if ((event.getX() >= CX-30 && event.getX() <= CX+30 && event.getY() >= 250 && event.getY() < 350) && Mch!=10){
+                    Currcol=Red1;
                 }
+
+
               break;
 
             case MotionEvent.ACTION_MOVE:
@@ -163,26 +172,37 @@ public class SOFview extends View {
 
     private void update() {
 
-        theta+=thcns*8;
-        if(theta>360|| theta < -360){theta=0;}
-        theta2+=thcns2;
-        if(theta2>360|| theta2 < -360){theta2=0;}
-        if(theta2> .95*theta && theta2< 1.05*theta ){
-            Mch=10;
-        }else {
-            Mch=0;
+        theta += thcns * 8;
+        if (theta > 360 || theta < -360) {
+            theta = 0;
         }
-        double Ex=B1dist* Math.cos(Math.toRadians(theta));
-        double Ey=B1dist* Math.sin(Math.toRadians(theta));
-        double E2x=B2dist* Math.cos(Math.toRadians(theta2));
-        double E2y=B2dist* Math.sin(Math.toRadians(theta2));
-        B1X =CX+ (float) Ex;
-        B1y =CY+(float) Ey;
-        B2X =CX+ (float) E2x;
-        B2y =CY+(float) E2y;
+        theta2 += thcns2;
+        if (theta2 > 360 || theta2 < -360) {
+            theta2 = 0;
+        }
+        if (theta2 > .95 * theta && theta2 < 1.05 * theta) {
+            Mch = 10;
+        } else {
+            Mch = 0;
+        }
+        double Ex = B1dist * Math.cos(Math.toRadians(theta));
+        double Ey = B1dist * Math.sin(Math.toRadians(theta));
+        double E2x = B2dist * Math.cos(Math.toRadians(theta2));
+        double E2y = B2dist * Math.sin(Math.toRadians(theta2));
+        B1X = CX + (float) Ex;
+        B1y = CY + (float) Ey;
+        B2X = CX + (float) E2x;
+        B2y = CY + (float) E2y;
 
-       }
-
+        if (Currcol != Blue1) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            Currcol = Blue1;
+        }
+    }
        // Called back when the view is first created or its size changes.
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
