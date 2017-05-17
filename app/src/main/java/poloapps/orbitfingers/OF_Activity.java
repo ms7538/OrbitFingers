206e464a93c3e21a11b030f589bbfaefd29b12a9
Marko <ms7538@gmail.com>
2017-05-17 16:04:53 -0700

    screen sizing continuous update

--- app/src/main/java/poloapps/orbitfingers/OF_Activity.java
+++ app/src/main/java/poloapps/orbitfingers/OF_Activity.java
@@ -6,10 +6,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import static java.security.AccessController.getContext;
 
 public class OF_Activity extends AppCompatActivity {
 
@@ -26,16 +29,30 @@ public class OF_Activity extends AppCompatActivity {
 
         float density = this.getResources().getDisplayMetrics().density;
 
-        Log.i("float density", String.valueOf(density) );
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
 
-        double scale_multiplier = density / 2;
+        Log.i("ob1 screen height", String.valueOf(height) );
+        Log.i("ob1 screen width", String.valueOf(width) );
+        Log.i("ob1 float density", String.valueOf(density) );
+        double scale_multiplier = density /2;
+/*
+        if( density > 2.0 ){
 
-        String plain = String.format( "%.1f", scale_multiplier );
+            scale_multiplier = density / 2;
 
-        Log.i("formatted density",plain);
+        }
+        else if ( density){
+
+        }*/
+        String format_SM_string = String.valueOf(scale_multiplier);
+
+       // Log.i("formatted density",format_SM_string);
         SharedPreferences.Editor editor = mSettings.edit();
 
-        editor.putString( "scale", plain );
+        editor.putString( "scale", format_SM_string );
 
         editor.commit();
 
