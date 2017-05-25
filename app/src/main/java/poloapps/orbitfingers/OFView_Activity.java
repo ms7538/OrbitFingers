1a579632ecf7ff9c7b1661a38ab483a36991aad1
Marko <ms7538@gmail.com>
2017-05-25 15:43:30 -0700

    V3.6 Ready for MySQL integration

--- app/src/main/java/poloapps/orbitfingers/OFView_Activity.java
+++ app/src/main/java/poloapps/orbitfingers/OFView_Activity.java
@@ -15,7 +15,7 @@ import java.util.Formatter;
 import android.graphics.Typeface;
 import android.widget.Toast;
 
-//V3.5 Created
+//V3.6 Created-- Ready for MySQL DB integration
 public class OFView_Activity extends View {
 
     SharedPreferences prefs = super.getContext().getSharedPreferences("Settings", 0); //
