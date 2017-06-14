44236915478dddbc09f119f41bf3c66b6dea4557
Marko Simunac <ms7538@gmail.com>
2017-06-14 00:34:35 -0700

    V3.6f Top 10 display in progress

--- app/src/main/java/poloapps/orbitfingers/UserAreaActivity.java
+++ app/src/main/java/poloapps/orbitfingers/UserAreaActivity.java
@@ -25,7 +25,7 @@ import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-//v3.6e Database Position Indication
+//v3.6f Top 10 Display in progress
 public class UserAreaActivity extends AppCompatActivity {
 
     final Handler handler = new Handler();
